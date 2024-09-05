49049ca92c23ec106840e840bf8897e82c967744
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-05 08:30:43 -0700

    And last pass to really clean up

--- custom_components/bart_realtime/const.py
+++ custom_components/bart_realtime/const.py
@@ -23,8 +23,6 @@ PLATFORMS = [BINARY_SENSOR, SENSOR]
 
 # Configuration and options
 CONF_ENABLED = "enabled"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
 CONF_API_KEY = "api_key"
 
 # Defaults
