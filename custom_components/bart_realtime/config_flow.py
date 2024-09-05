eb2bfdf896388d6fe41aee10e298deb43c9d5c2f
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-05 10:18:03 -0700

    More small changes needed

--- custom_components/bart_realtime/config_flow.py
+++ custom_components/bart_realtime/config_flow.py
@@ -106,5 +106,5 @@ class BartRealtimeOptionsFlowHandler(config_entries.OptionsFlow):
     async def _update_options(self):
         """Update config entry options."""
         return self.async_create_entry(
-            title=self.config_entry.data.get(CONF_API_KEY), data=self.options
+            title=self.config_entry.data.get(CONF_STATION), data=self.options
         )
--- tests/const.py
+++ tests/const.py
@@ -1,6 +1,7 @@
 """Constants for Bart Realtime tests."""
 from custom_components.bart_realtime.const import (
     CONF_API_KEY,
+    CONF_STATION
 )
 
-MOCK_CONFIG = {CONF_API_KEY: "test_api_key"}
+MOCK_CONFIG = {CONF_API_KEY: "test_api_key", CONF_STATION: "16TH"}
