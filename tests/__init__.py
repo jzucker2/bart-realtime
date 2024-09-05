7e2f0e27883bfaab42c7e1391b8a2d10e5ce5ec1
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-04 19:18:06 -0700

    First commit

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Bart Realtime integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Bart Realtime tests."""
+from custom_components.bart_realtime.const import (
+    CONF_PASSWORD,
+)
+from custom_components.bart_realtime.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
