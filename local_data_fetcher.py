4114f643efe7ebf6c0eb9b2b2c90a6f40ff01556
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-05 08:48:11 -0700

    Clean up that new thing too

--- local_data_fetcher.py
+++ local_data_fetcher.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-
 import asyncio
 import pprint
 
 import aiohttp
-
 from custom_components.bart_realtime.api import BartRealtimeApiClient
+
+
+API_KEY = "api_key"
 
 
 async def main():
     async with aiohttp.ClientSession() as client:
-        bart_api = BartRealtimeApiClient("api_key", client)
+        bart_api = BartRealtimeApiClient(API_KEY, client)
         games = await bart_api.async_get_sanitized_train_times()
         pprint.pprint(games)
         print(len(games))
