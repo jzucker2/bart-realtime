4bbb4df8222a9fd0931c51cffc7b9a7c08de4cfd
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-05 17:04:48 -0700

    I might actually be getting somewhere

--- custom_components/bart_realtime/entity.py
+++ custom_components/bart_realtime/entity.py
@@ -13,14 +13,18 @@ class BartRealtimeEntity(CoordinatorEntity):
         self.config_entry = config_entry
 
     @property
+    def config_entry_id(self):
+        return self.config_entry.entry_id
+
+    @property
     def unique_id(self):
         """Return a unique ID to use for this entity."""
-        return self.config_entry.entry_id
+        return self.config_entry_id
 
     @property
     def device_info(self):
         return {
-            "identifiers": {(DOMAIN, self.unique_id)},
+            "identifiers": {(DOMAIN, self.config_entry_id)},
             "name": NAME,
             "model": VERSION,
             "manufacturer": NAME,
