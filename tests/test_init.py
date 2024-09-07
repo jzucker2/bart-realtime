51073bd591d3ff6e03d27a4060584efb9e317b6b
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-07 15:01:40 -0700

    Trying to just have tests pass for now

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,7 +2,6 @@
 
 from homeassistant.exceptions import ConfigEntryNotReady
 import pytest
-import pytest_asyncio  # noqa: F401
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.bart_realtime import (
@@ -21,7 +20,6 @@ from .const import MOCK_CONFIG
 # Home Assistant using the pytest_homeassistant_custom_component plugin.
 # Assertions allow you to verify that the return value of whatever is on the left
 # side of the assertion matches with the right side.
-@pytest.mark.asyncio
 async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
     """Test entry setup and unload."""
     # Create a mock entry so we don't have to go through config flow
@@ -48,7 +46,6 @@ async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
     assert config_entry.entry_id not in hass.data[DOMAIN]
 
 
-@pytest.mark.asyncio
 async def test_setup_entry_exception(hass, error_on_get_data):
     """Test ConfigEntryNotReady when API raises an exception during entry setup."""
     config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
