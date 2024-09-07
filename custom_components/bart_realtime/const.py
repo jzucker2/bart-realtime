7400bbc5aefa8e0965f24b84d3b624f469befddf
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-07 14:00:22 -0700

    More quotes

--- custom_components/bart_realtime/const.py
+++ custom_components/bart_realtime/const.py
@@ -29,7 +29,7 @@ CONF_STATION = "station"
 # Defaults
 DEFAULT_NAME = DOMAIN
 
-MISSING_VALUE = 'missing'
+MISSING_VALUE = "missing"
 
 
 STARTUP_MESSAGE = f"""
@@ -46,4 +46,4 @@ DEFAULT_BART_STATION = "16TH"
 
 # https://api.bart.gov/docs/etd/etd.aspx
 # https://api.bart.gov/api/etd.aspx?cmd=etd&orig=16TH&key=MW9S-E7SL-26DU-VV8V
-DEFAULT_BART_API_BASE_URL = 'https://api.bart.gov/api/etd.aspx?cmd=etd&orig=16TH&key=MW9S-E7SL-26DU-VV8V&json=y'
+DEFAULT_BART_API_BASE_URL = "https://api.bart.gov/api/etd.aspx?cmd=etd&orig=16TH&key=MW9S-E7SL-26DU-VV8V&json=y"
