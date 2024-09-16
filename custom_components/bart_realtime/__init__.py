f48606d38840f7119b53679d0fd35a8e32b389a6
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-15 18:41:46 -0700

    Good enough for now

--- custom_components/bart_realtime/__init__.py
+++ custom_components/bart_realtime/__init__.py
@@ -9,7 +9,7 @@ import asyncio
 from dataclasses import dataclass
 from datetime import timedelta
 import logging
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
@@ -29,8 +29,8 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 type BartRealtimeConfigEntry = ConfigEntry[BartRealtimeData]
 
 
-class BartCoordinators(NamedTuple):
-    """Tuya data stored in the Home Assistant data object."""
+class BartUpdateCoordinators(NamedTuple):
+    """Bart update coordinators stored in the Home Assistant runtime_data object."""
 
     trains_coordinator: BartRealtimeDataUpdateCoordinator
 
@@ -40,6 +40,7 @@ class BartRealtimeData:
     api_key: str
     station: str
     client: BartRealtimeApiClient
+    coordinators: Optional[BartUpdateCoordinators] = None
 
     @classmethod
     def from_entry(cls, entry: BartRealtimeConfigEntry, session):
@@ -50,8 +51,12 @@ class BartRealtimeData:
         station = entry.data.get(CONF_STATION)
 
         client = BartRealtimeApiClient(api_key, station, session)
-
         return cls(api_key=api_key, station=station, client=client)
+
+    def set_coordinators(self, trains_update_coordinator):
+        self.coordinators = BartUpdateCoordinators(
+            train_update_coordinator=trains_update_coordinator,
+        )
 
 
 async def async_setup(hass: HomeAssistant, config: Config):
@@ -64,8 +69,7 @@ async def async_setup_entry(
     entry: BartRealtimeConfigEntry,
 ):
     """Set up this integration using UI."""
-    if hass.data.get(DOMAIN) is None:
-        hass.data.setdefault(DOMAIN, {})
+    if entry.runtime_data is None:
         _LOGGER.info(STARTUP_MESSAGE)
 
     session = async_get_clientsession(hass)
@@ -74,17 +78,18 @@ async def async_setup_entry(
     # Assign the runtime_data
     entry.runtime_data = data
 
-    coordinator = BartRealtimeDataUpdateCoordinator(hass, client=data.client)
-    await coordinator.async_refresh()
+    trains_coordinator = BartRealtimeDataUpdateCoordinator(hass, client=data.client)
+    await trains_coordinator.async_refresh()
 
-    if not coordinator.last_update_success:
+    if not trains_coordinator.last_update_success:
         raise ConfigEntryNotReady
 
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    # hass.data[DOMAIN][entry.entry_id] = trains_coordinator
+    data.set_coordinators(trains_coordinator)
 
-    coordinator.set_platforms(PLATFORMS)
+    trains_coordinator.set_platforms(PLATFORMS)
     # https://developers.home-assistant.io/blog/2024/03/13/deprecate_add_run_job
-    hass.async_add_job(hass.config_entries.async_forward_entry_setups(entry, PLATFORMS))
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.add_update_listener(async_reload_entry)
     return True
@@ -116,5 +121,4 @@ async def async_reload_entry(
     entry: BartRealtimeConfigEntry,
 ) -> None:
     """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+    await hass.config_entries.async_reload(entry.entry_id)
