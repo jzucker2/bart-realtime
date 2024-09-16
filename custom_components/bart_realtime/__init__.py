be4c4286faa3418a6406f6eda63799ee5d8c72e2
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-15 19:13:09 -0700

    This is a lot of changes

--- custom_components/bart_realtime/__init__.py
+++ custom_components/bart_realtime/__init__.py
@@ -8,7 +8,7 @@ https://github.com/jzucker2/bart-realtime
 from dataclasses import dataclass
 from datetime import timedelta
 import logging
-from typing import NamedTuple, Optional
+from typing import NamedTuple
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
@@ -35,26 +35,43 @@ class BartUpdateCoordinators(NamedTuple):
 
 
 @dataclass
-class BartRealtimeData:
+class BartRealtimeEntryConfigData:
     api_key: str
     station: str
-    client: BartRealtimeApiClient
-    coordinators: Optional[BartUpdateCoordinators] = None
 
     @classmethod
-    def from_entry(cls, entry: BartRealtimeConfigEntry, session):
+    def from_entry(cls, entry: BartRealtimeConfigEntry):
         _LOGGER.debug(
             "Processing data config entry: %s with entry.data: %s", entry, entry.data
         )
         api_key = entry.data.get(CONF_API_KEY)
         station = entry.data.get(CONF_STATION)
+        return cls(api_key=api_key, station=station)
 
-        client = BartRealtimeApiClient(api_key, station, session)
-        return cls(api_key=api_key, station=station, client=client)
 
-    def set_coordinators(self, trains_update_coordinator):
-        self.coordinators = BartUpdateCoordinators(
-            train_update_coordinator=trains_update_coordinator,
+@dataclass
+class BartRealtimeData:
+    entry_config_data: BartRealtimeEntryConfigData
+    client: BartRealtimeApiClient
+    coordinators: BartUpdateCoordinators
+
+    @classmethod
+    def from_entry(cls, hass, entry_config_data: BartRealtimeEntryConfigData):
+        _LOGGER.debug("Processing data config entry_config_data: %s", entry_config_data)
+
+        session = async_get_clientsession(hass)
+
+        client = BartRealtimeApiClient(
+            entry_config_data.api_key, entry_config_data.station, session
+        )
+
+        trains_coordinator = BartRealtimeDataUpdateCoordinator(hass, client)
+        coordinators = BartUpdateCoordinators(trains_coordinator=trains_coordinator)
+
+        return cls(
+            entry_config_data=entry_config_data,
+            client=client,
+            coordinators=coordinators,
         )
 
     @property
@@ -75,22 +92,21 @@ async def async_setup_entry(
     if entry.runtime_data is None:
         _LOGGER.info(STARTUP_MESSAGE)
 
-    session = async_get_clientsession(hass)
-    data = BartRealtimeData.from_entry(entry, session)
+    entry_config_data = BartRealtimeEntryConfigData.from_entry(entry)
+
+    data = BartRealtimeData.from_entry(hass, entry_config_data)
 
     # Assign the runtime_data
     entry.runtime_data = data
 
-    trains_coordinator = BartRealtimeDataUpdateCoordinator(hass, client=data.client)
+    trains_coordinator = data.trains_coordinator
     await trains_coordinator.async_refresh()
 
     if not trains_coordinator.last_update_success:
         raise ConfigEntryNotReady
 
-    # hass.data[DOMAIN][entry.entry_id] = trains_coordinator
-    data.set_coordinators(trains_coordinator)
-
     trains_coordinator.set_platforms(PLATFORMS)
+
     # Set up all platforms for this device/entry.
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
